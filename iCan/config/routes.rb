4e868aecfd4aa744b05f99c254ff71718cc7e35a
DarkPotatoKing <kyle.rosales97@gmail.com>
2015-12-29 19:26:55 +0800

    setup route

--- iCan/config/routes.rb
+++ iCan/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'pages/index'
 
   root to: 'pages#index'
+  resources :sessions, only: [:update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
