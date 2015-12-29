e225b3b07ca681ca3bd8a70187f3d3ed81cd8e06
DarkPotatoKing <kyle.rosales97@gmail.com>
2015-12-29 22:50:02 +0800

    finish project

--- iCan/app/models/ability.rb
+++ iCan/app/models/ability.rb
@@ -8,9 +8,12 @@ class Ability
       can :manage, :all
     elsif user.role?(:moderator)
       can :create, Project
+      can :update, Project do |project|
+        project.ongoing?
+      end
       can :read, Project
     elsif user.role?(:user)
-      can :read, Project
+      can :read, Project, ongoing: true
     end
   end
 end
